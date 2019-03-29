46b94338133b11f09dd734788cacd4c288c9d986
Helmy <chanhelmy@hotmail.com>
2019-03-29 09:37:31 +0000

    make minor changes to map.js

--- map.js
+++ map.js
@@ -2,9 +2,13 @@
    global google
    global $
  */
- function initMap() {
-           map = new google.maps.Map($('#map')[0], {
-          center: {lat: 1.35, lng: 103.8},
-          zoom: 10
-        });
+let map = null;
+function initMap(){
+
+          //Create the google coordinate
+            let singapore = new google.maps.LatLng(1.35, 103.8);
+            
+            //Create the google map
+            map = new google.maps.Map(
+            document.getElementById('map'), {center: singapore, zoom: 12});
 }
